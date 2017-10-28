c8abc4e0ccaa61ad90df6165491cf53ca41b912b
Christoffer Persson <christoffer.g.persson@gmail.com>
2017-10-28 16:46:48 +0200

    add soil moisture sensor

--- ioTemp.ino
+++ ioTemp.ino
@@ -1,23 +1,33 @@
 #include <Thread.h>
 #include <ThreadController.h>
 
-#define LOW_TEMP_THRESHOLD 25
+#define LOW_TEMP_THRESHOLD 19
+
+int CALIBRATION_ANALOG_HIGH_READING 970;
+int CALIBRATION_ANALOG_LOW_READING 320;
 
 
 ThreadController threadController = ThreadController();
 
 Thread* tempThread = new Thread();
+Thread* soilThread = new Thread();
 
 void setup(void) {
   Serial.begin(115200);
 
   setupWifi();
   setupDS18B20(LOW_TEMP_THRESHOLD);
+  setupSoil(CALIBRATION_ANALOG_HIGH_READING,
+            CALIBRATION_ANALOG_LOW_READING);
 
   tempThread->onRun(runTemp);
   tempThread->setInterval(10000);
 
+  soilThread->onRun(runSoil);
+  soilThread->setInterval(1000);
+
   threadController.add(tempThread);
+  threadController.add(soilThread);
 
 }
 
---
+++ soil.ino
@@ -0,0 +1,34 @@
+#define ANALOG_IN A0
+
+int _calibrationAnalogHighReading 1024;
+int _calibrationAnalogLowReading 0;
+
+int _lastSoilMoisture;
+
+void setupSoil(int calibrationAnalogHighReading,
+               int calibrationAnalogLowReadirng) {
+  _calibrationAnalogHighReading = calibrationAnalogHighReading;
+  _calibrationAnalogLowReading = calibrationAnalogLowReadirng;
+}
+
+void runSoil(void) {
+  updateSoilMoisture();
+  int soilMoisture = getSoilMoisture();
+  Serial.print("Soil moisture is: ");
+  Serial.print(soilMoisture);
+  Serial.println("%");
+}
+
+void updateSoilMoisture(void) {
+  int analogOutput = analogRead(ANALOG_IN);
+  Serial.print("updateSoilMoisture analogOutput: ");
+  Serial.println(analogOutput);
+
+  _lastSoilMoisture = map(analogOutput,
+                          _calibrationAnalogHighReading, _calibrationAnalogLowReading,
+                          0, 100);
+}
+
+float getSoilMoisture(void) {
+  return _lastSoilMoisture;
+}
